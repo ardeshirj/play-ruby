e9bd7ec130c0cc621b62bb6d85e9def463c11bb6
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-02-08 12:46:26 -0800

    Add bottels method

--- play.rb
+++ play.rb
@@ -32,6 +32,15 @@ module PlayModule
       end
     end
   end
+
+  def self.bottels(n)
+    if n == 0
+      puts 'no more bottles of beer on the wall'
+    else
+      puts "#{n} bottles of beer on the wall"
+      bottels(n - 1)
+    end
+  end
 end
 
 # RubyMonk
